45673e77f6a1633ea23512b4343460189086adfc
mohsinmx <mohsinx.mohammad@intel.com>
2022-11-25 12:37:49 +0530

    Update OV 2022.3 Changes

--- onnxruntime/core/providers/openvino/ov_versions/capability.cc
+++ onnxruntime/core/providers/openvino/ov_versions/capability.cc
@@ -26,15 +26,16 @@ namespace openvino_ep {
 GetCapability::GetCapability(const GraphViewer& graph_viewer_param, std::string device_type_param,
                              const std::string version_param):
                 graph_viewer_(graph_viewer_param), device_type_(device_type_param){
-  if (version_param == "V_2021_4") {
-    data_ops_ = new DataOps(graph_viewer_, V_2021_4, device_type_);
-  } else if (version_param == "V_2022_1") {
+  if (version_param == "V_2022_1") {
     data_ops_ = new DataOps(graph_viewer_, V_2022_1, device_type_);
   } else if (version_param == "V_2022_2") {
     data_ops_ = new DataOps(graph_viewer_, V_2022_2, device_type_);
+  } else if (version_param == "V_2022_3") {
+    data_ops_ = new DataOps(graph_viewer_, V_2022_3, device_type_);
   } else {
-    data_ops_ = new DataOps(graph_viewer_, V_2022_2, device_type_);
+    data_ops_ = new DataOps(graph_viewer_, V_2022_3, device_type_);
   }
+
 }
 
 std::vector<std::unique_ptr<ComputeCapability>> GetCapability::Execute() {
