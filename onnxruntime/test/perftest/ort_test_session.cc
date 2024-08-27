1e3dadd355c1bcd665e83d61a6dc8addc5399ccd
Eric Crawford <eric.r.crawford@intel.com>
2024-08-27 10:26:28 -0700

    Revert "Hard code onnx perf to use RT NPU for outputs"
    This reverts commit c1f3b3ec7caee7024b0ef887fa762d36001e0daa.

--- onnxruntime/test/perftest/ort_test_session.cc
+++ onnxruntime/test/perftest/ort_test_session.cc
@@ -838,10 +838,7 @@ select from 'TF8', 'TF16', 'UINT8', 'FLOAT', 'ITENSOR'. \n)");
   }
   size_t output_count = session_.GetOutputCount();
   output_names_.resize(output_count);
-
-  Ort::MemoryInfo memory_info = Ort::MemoryInfo("OpenVINO_RT_NPU", OrtArenaAllocator, 0, OrtMemTypeCPUOutput);
-  Ort::Allocator a(session_, memory_info);
-
+  Ort::AllocatorWithDefaultOptions a;
   for (size_t i = 0; i != output_count; ++i) {
     auto output_name = session_.GetOutputNameAllocated(i, a);
     assert(output_name != nullptr);
