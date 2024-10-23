1c122bbd3452f57593d755ab9d64e05e7f4c6329
Satya Kumar Jandhyala <satya.k.jandhyala@gmail.com>
2024-10-23 10:14:09 -0700

    [JS/WebGPU] GroupQueryAttention rewrite (#20946)
    ### Description
    Implement JSEP GroupQueryAttention
    
    
    
    ### Motivation and Context
    Required to enable certain LLM models to run using WebGPU.

--- js/web/lib/wasm/jsep/webgpu/op-resolve-rules.ts
+++ js/web/lib/wasm/jsep/webgpu/op-resolve-rules.ts
@@ -20,7 +20,6 @@ import { gatherND, parseGatherNDAttributes } from './ops/gather-nd';
 import { gatherBlockQuantized, parseGatherBlockQuantizedAttributes } from './ops/gather-block-quantized';
 import { gatherElements, parseGatherElementsAttributes } from './ops/gather-elements';
 import { gemm, parseGemmAttributes } from './ops/gemm';
-import { gridSample, parseGridSampleAttributes } from './ops/grid-sample';
 import { groupQueryAttention } from './ops/group-query-attention';
 import { instanceNorm } from './ops/instance-norm';
 import { layerNorm } from './ops/layer-norm';
@@ -108,7 +107,6 @@ export const WEBGPU_OP_RESOLVE_RULES: Map<string, OperatorImplementation> = new 
   ['GlobalMaxPool', [pool.globalMaxPool, pool.parseGlobalMaxPoolAttributes]],
   ['Greater', [binaryOps.greater]],
   ['GreaterOrEqual', [binaryOps.greaterOrEqual]],
-  ['GridSample', [gridSample, parseGridSampleAttributes]],
   ['GroupQueryAttention', [groupQueryAttention]],
   ['HardSigmoid', [unaryOps.hardSigmoid, unaryOps.parseHardSigmoidAttributes]],
   ['InstanceNormalization', [instanceNorm]],
