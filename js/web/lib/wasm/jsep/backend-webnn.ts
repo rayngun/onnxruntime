c85e155ceddcf3d56f0fb8a7698c89e26a729b58
Wanming Lin <wanming.lin@intel.com>
2024-10-26 08:44:46 +0800

    [WebNN] Support int4 and uint4 data types (#22575)

--- js/web/lib/wasm/jsep/backend-webnn.ts
+++ js/web/lib/wasm/jsep/backend-webnn.ts
@@ -277,6 +277,8 @@ export class WebNNBackend {
       case 'int8':
         bufferView = new Int8Array(buffer);
         break;
+      case 'int4':
+      case 'uint4':
       case 'uint8':
         bufferView = new Uint8Array(buffer);
         break;
