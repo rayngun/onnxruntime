a565a68b923468ee37c8d228e2a69e3ac7bd8802
Wanming Lin <wanming.lin@intel.com>
2024-10-31 08:52:10 +0800

    [WebNN] Support And, Or and Xor ops (#22598)
    Co-authored-by: Dwayne Robinson <fdwr@hotmail.com>

--- onnxruntime/core/providers/webnn/builders/op_builder_factory.cc
+++ onnxruntime/core/providers/webnn/builders/op_builder_factory.cc
@@ -141,10 +141,6 @@ static OpBuilderRegistrations CreateOpBuilderRegistrations() {
     CreateLogicalOpBuilder("Xor", op_registrations);
   }
 
-  {  // LRN
-    CreateLRNOpBuilder("LRN", op_registrations);
-  }
-
   {  // LSTM
     CreateLstmOpBuilder("LSTM", op_registrations);
   }
